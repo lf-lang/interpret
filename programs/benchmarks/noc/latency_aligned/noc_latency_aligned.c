841e500a0c113754c3fd414d0089134f31421f42
Peter Donovan <peterdonovan@berkeley.edu>
2022-12-30 20:19:16 -0800

    store->nop->load -> wrong WB read. Bug?

--- programs/benchmarks/noc/latency_aligned/noc_latency_aligned.c
+++ programs/benchmarks/noc/latency_aligned/noc_latency_aligned.c
@@ -22,10 +22,6 @@ int main() {
 
 static int send_main(uint32_t receiver) {
     asm volatile (
-        // "li t1, 0x40000004\n\t"  // wishbone address mmio address
-        // "li t2, 0x40000008\n\t"  // wishbone data mmio address
-        // "li t3, 0x08\n\t"  // noc destination wishbone address
-        // "li t4, 0x04\n\t"  // noc data wishbone address
         "li t4, 0x40000000\n\t"
         // Set noc destination to 1
         "li t5, 0x08\n\t"  // noc destination wishbone address
@@ -33,8 +29,6 @@ static int send_main(uint32_t receiver) {
         "li t5, 0x1\n\t"  // noc destination
         "sw t5, 8(t4)\n\t"
         // Check that the wishbone has accepted the write
-        "nop\n\t"
-        "nop\n\t"
         "lw t5, 16(t4)\n\t"
         "li t4, 0xbaaabaaa\n\t"
         "csrw 0x51e, t4\n\t"
@@ -50,16 +44,6 @@ static int send_main(uint32_t receiver) {
         "sw t5, 4(t4)\n\t"
         "li t5, 0x1\n\t"
         "sw t5, 8(t4)\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        // Is it necessary to wait here to make sure the destination was passed down to the NoC?
-        // "li t5, 42\n\t"  // noc value
-        // "sw t5, 4(x0)\n\t"  // noc data
-        // "rdcycle t1\n\t"
-        // "andi t1, t1, 1023\n\t"
-        // "li t0, 0xbaaabaaa\n\t"
-        // "csrw 0x51e, t0\n\t"
-        // "csrw 0x51e, t1\n\t"
     );
 }
 
@@ -69,6 +53,7 @@ static int receive_main(uint32_t sender) {
         "li t4, 0x40000000\n\t"  // wishbone base address
         "CHECK_IF_RECEIVED_YET:\n\t"
         "sw x0, 0(t4)\n\t"  // Write the address of NoC CSR to Wishbone read address
+        "nop\n\t"
         "lw t5, 12(t4)\n\t"  // Read NoC CSR
         "beq x0, t5, CHECK_IF_RECEIVED_YET\n\t"
         "rdcycle t3\n\t"
