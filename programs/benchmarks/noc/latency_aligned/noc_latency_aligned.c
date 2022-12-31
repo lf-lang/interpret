f9afb8663d7e9925329cf4c0d62d16c42ca09b57
Peter Donovan <peterdonovan@berkeley.edu>
2022-12-31 14:06:33 -0800

    Actually 35 cycles of latency it seems.

--- programs/benchmarks/noc/latency_aligned/noc_latency_aligned.c
+++ programs/benchmarks/noc/latency_aligned/noc_latency_aligned.c
@@ -21,40 +21,52 @@ int main() {
 }
 
 static int send_main(uint32_t receiver) {
+    _fp_print(wb_read(NOC_CSR));
     asm volatile (
         "li t4, 0x40000000\n\t"
         // Set noc destination to 1
-        "li t5, 0x08\n\t"  // noc destination wishbone address
-        "sw t5, 4(t4)\n\t"
-        "li t5, 0x1\n\t"  // noc destination
-        "sw t5, 8(t4)\n\t"
-        // Check that the wishbone has accepted the write
-        "lw t5, 16(t4)\n\t"
-        "li t4, 0xbaaabaaa\n\t"
-        "csrw 0x51e, t4\n\t"
-        "csrw 0x51e, t5\n\t"  // should be nonzero
-        "li t4, 0x40000000\n\t"
+        // "li t5, 0x08\n\t"  // noc destination wishbone address
+        // "sw t5, 4(t4)\n\t"
+        // "li t5, 0x1\n\t"  // noc destination
+        // "sw t5, 8(t4)\n\t"
+        // // Check that the wishbone has accepted the write
+        // "lw t5, 16(t4)\n\t"
+        // "li t4, 0xbaaabaaa\n\t"
+        // "csrw 0x51e, t4\n\t"
+        // "csrw 0x51e, t5\n\t"  // should be nonzero
+        // "li t4, 0x40000000\n\t"
         WAIT_FOR_NEXT_ZERO_MOD_1024(send)  // clobber "a" registers, as well as t0, t1, t6
         // like noc_send, but without blocking
-        "li t5, 0x04\n\t"  // Set noc data to 42
-        "sw t5, 4(t4)\n\t"
-        "li t5, 42\n\t"
+        "li t5, 0x1\n\t"  // noc destination
         "sw t5, 8(t4)\n\t"
-        "li t5, 0x08\n\t"  // noc destination
+        "li t5, 0x08\n\t"
         "sw t5, 4(t4)\n\t"
-        "li t5, 0x1\n\t"
-        "sw t5, 8(t4)\n\t"
+        "nop\n\t"
+        "nop\n\t"
+        "li t5, 42\n\t"  // Set noc data to 42
+        "sw t5, 8(t4)\n\t" // FIXME: Data must be written first? Why? Is it Hardware Bug?
+        "li t5, 0x04\n\t"
+        "sw t5, 4(t4)\n\t"
     );
 }
 
 static int receive_main(uint32_t sender) {
+    // asm volatile(
+    //     "nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t"
+    // );
+    // while (1) _fp_print(wb_read(NOC_CSR));
     asm volatile (
         WAIT_FOR_NEXT_ZERO_MOD_1024(receive)
         "li t4, 0x40000000\n\t"  // wishbone base address
         "CHECK_IF_RECEIVED_YET:\n\t"
         "sw x0, 0(t4)\n\t"  // Write the address of NoC CSR to Wishbone read address
         "nop\n\t"
+        "nop\n\t"
         "lw t5, 12(t4)\n\t"  // Read NoC CSR
+        // "li t0, 0xbaaabaaa\n\t"
+        // "csrw 0x51e, t0\n\t"
+        // "csrw 0x51e, t5\n\t"
+        "andi t5, t5, 2\n\t"
         "beq x0, t5, CHECK_IF_RECEIVED_YET\n\t"
         "rdcycle t3\n\t"
         "andi t3, t3, 1023\n\t"
