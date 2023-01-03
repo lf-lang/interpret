93e16318363b90af7a4801658d521d971da7d75a
Peter Donovan <peterdonovan@berkeley.edu>
2023-01-02 20:52:23 -0800

    Get a basic test working in simulation.
    On the order of 12 cycles worst case to send an integer without
    handshaking except checking the valid bit.

--- programs/noc/LowLevelInterface/low_level_interface_noc.c
+++ programs/noc/LowLevelInterface/low_level_interface_noc.c
@@ -13,73 +13,29 @@ int main() {
         case 1: main1(); break;
         case 2: main1(); break;
         case 3: main1(); break;
-        default: _fp_print(66); //ERROR
+        default: _fp_print(666); //ERROR
     }
 }
 
 int main0() {
     asm volatile(
         "li t4, 0x80000000\n\t"
-        "li t2, 42\n\t"
-        "li t3, 43\n\t"
-        "li a1, 44\n\t"
-        "li t5, 45\n\t"
-        "li t6, 46\n\t"
+        "rdcycle t2\n\t"
         "sw t2, 0(t4)\n\t"
-        "sw t3, 0(t4)\n\t"
-        "sw a1, 0(t4)\n\t"
-        "sw t5, 0(t4)\n\t"
-        "sw t6, 0(t4)\n\t"
         "sw t2, 0(t4)\n\t"
-        "sw t3, 0(t4)\n\t"
-        "sw a1, 0(t4)\n\t"
-        "sw t5, 0(t4)\n\t"
-        "sw t6, 0(t4)\n\t"
+        "sw t2, 0(t4)\n\t"
+        "sw t2, 0(t4)\n\t"
     );
 }
 
 int main1() {
     asm volatile(
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
         "li t4, 0x80000000\n\t"
-        "lw t3, 0(t4)\n\t"
-        "li t0, 0xbaaabaaa\n\t"
-        "csrw 0x51e, t0\n\t"
-        "csrw 0x51e, t3\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "nop\n\t"
-        "li t4, 0x80000000\n\t"
-        "lw t3, 0(t4)\n\t"
+        "POLL: lw t3, 16(t4)\n\t"
+        "beq x0, t3, POLL\n\t"
+        "rdcycle t3\n\t"
+        "lw t2, 0(t4)\n\t"
+        "sub t3, t3, t2\n\t"
         "li t0, 0xbaaabaaa\n\t"
         "csrw 0x51e, t0\n\t"
         "csrw 0x51e, t3\n\t"
