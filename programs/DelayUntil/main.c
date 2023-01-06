925c5ae94b2a861eb068d3a10e37c4a0aab8a641
Erling Rennemo Jellum <erling.r.jellum@ntnu.no>
2023-01-05 19:50:59 -0800

    Add delay_until example and bump FP

---
+++ programs/DelayUntil/main.c
@@ -0,0 +1,17 @@
+#include <flexpret_io.h>
+#include <flexpret_csrs.h>
+
+int main() {
+    _fp_print(1);
+    int core_id = read_csr(CSR_COREID);
+    _fp_print(core_id);
+
+    if (core_id != 0) {
+        return 0;
+    }
+
+    unsigned int pre = rdtime();
+    delay_for(10000);
+    unsigned int post = rdtime();
+    _fp_print(post-pre);
+}
