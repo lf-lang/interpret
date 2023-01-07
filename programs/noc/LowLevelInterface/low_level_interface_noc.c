d5983a3a813cbc8e034205ff4a7e2fdd6a2e1ddf
Peter Donovan <peterdonovan@berkeley.edu>
2023-01-06 18:54:44 -0800

    Bugfix; move header-only lib to flexpret.

--- programs/noc/LowLevelInterface/low_level_interface_noc.c
+++ programs/noc/LowLevelInterface/low_level_interface_noc.c
@@ -1,9 +1,6 @@
 #include <stdint.h>
 #include <flexpret_io.h>
-
-#include "asm_utils.h"
-#include "low_level_interface.h"
-#include "c_api.h"
+#include <noc/flexpret_noc_c_api.h>
 
 /* | NE | _ | N | E | _ | */
 
@@ -30,29 +27,29 @@ int main() {
 }
 
 int main0() {
-    broadcast_count(0, 1023);
-    // read_n_words_and_print(1, EAST_INT);
-    // read_n_words_and_print(2, NORTH_INT);
-    // read_n_words_and_print(3, NORTHEAST_INT);
+    broadcast_count(0, 125);
+    read_n_words_and_print(1, EAST_INT);
+    read_n_words_and_print(2, NORTH_INT);
+    read_n_words_and_print(3, NORTHEAST_INT);
 }
 
 int main1() {
     read_n_words_and_print(0, EAST_INT);
-    // broadcast_count(1, 17);
-    // read_n_words_and_print(2, NORTHEAST_INT);
-    // read_n_words_and_print(3, NORTH_INT);
+    broadcast_count(1, 17);
+    read_n_words_and_print(2, NORTHEAST_INT);
+    read_n_words_and_print(3, NORTH_INT);
 }
 
 int main2() {
     read_n_words_and_print(0, NORTH_INT);
-    // read_n_words_and_print(1, NORTHEAST_INT);
-    // broadcast_count(2, 42);
-    // read_n_words_and_print(3, EAST_INT);
+    read_n_words_and_print(1, NORTHEAST_INT);
+    broadcast_count(2, 42);
+    read_n_words_and_print(3, EAST_INT);
 }
 
 int main3() {
     read_n_words_and_print(0, NORTHEAST_INT);
-    // read_n_words_and_print(1, NORTH_INT);
-    // read_n_words_and_print(2, EAST_INT);
-    // broadcast_count(3, 3);
+    read_n_words_and_print(1, NORTH_INT);
+    read_n_words_and_print(2, EAST_INT);
+    broadcast_count(3, 3);
 }
