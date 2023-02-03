b18fd087b3346a8f7e9189234bf5ff0450139940
Peter Donovan <peterdonovan@berkeley.edu>
2023-02-02 23:05:30 -0800

    Assembly generation "hello world".

--- programs/noc/BroadcastMemory/broadcast_memory_noc.c
+++ programs/noc/BroadcastMemory/broadcast_memory_noc.c
@@ -16,40 +16,41 @@ int main2();
 int main3();
 
 int main() {
-    int core_id = read_csr(CSR_COREID);
-    switch(core_id) {
-        case 0: main0(); break;
-        case 1: main1(); break;
-        case 2: main2(); break;
-        case 3: main3(); break;
-        default: _fp_print(666); //ERROR
-    }
+    hello();
+    // int core_id = read_csr(CSR_COREID);
+    // switch(core_id) {
+    //     case 0: main0(); break;
+    //     case 1: main1(); break;
+    //     case 2: main2(); break;
+    //     case 3: main3(); break;
+    //     default: _fp_print(666); //ERROR
+    // }
 }
 
-int main0() {
-    broadcast_count(0, 125);
-    read_n_words_and_print(1, EAST_INT);
-    read_n_words_and_print(2, NORTH_INT);
-    read_n_words_and_print(3, NORTHEAST_INT);
-}
+// int main0() {
+//     broadcast_count(0, 125);
+//     read_n_words_and_print(1, EAST_INT);
+//     read_n_words_and_print(2, NORTH_INT);
+//     read_n_words_and_print(3, NORTHEAST_INT);
+// }
 
-int main1() {
-    read_n_words_and_print(0, EAST_INT);
-    broadcast_count(1, 17);
-    read_n_words_and_print(2, NORTHEAST_INT);
-    read_n_words_and_print(3, NORTH_INT);
-}
+// int main1() {
+//     read_n_words_and_print(0, EAST_INT);
+//     broadcast_count(1, 17);
+//     read_n_words_and_print(2, NORTHEAST_INT);
+//     read_n_words_and_print(3, NORTH_INT);
+// }
 
-int main2() {
-    read_n_words_and_print(0, NORTH_INT);
-    read_n_words_and_print(1, NORTHEAST_INT);
-    broadcast_count(2, 42);
-    read_n_words_and_print(3, EAST_INT);
-}
+// int main2() {
+//     read_n_words_and_print(0, NORTH_INT);
+//     read_n_words_and_print(1, NORTHEAST_INT);
+//     broadcast_count(2, 42);
+//     read_n_words_and_print(3, EAST_INT);
+// }
 
-int main3() {
-    read_n_words_and_print(0, NORTHEAST_INT);
-    read_n_words_and_print(1, NORTH_INT);
-    read_n_words_and_print(2, EAST_INT);
-    broadcast_count(3, 3);
-}
+// int main3() {
+//     read_n_words_and_print(0, NORTHEAST_INT);
+//     read_n_words_and_print(1, NORTH_INT);
+//     read_n_words_and_print(2, EAST_INT);
+//     broadcast_count(3, 3);
+// }
