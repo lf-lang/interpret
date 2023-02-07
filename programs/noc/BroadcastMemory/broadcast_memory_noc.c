f066f422ee09c32968776f741aad5aee58b62ee3
Peter Donovan <peterdonovan@berkeley.edu>
2023-02-06 21:14:44 -0800

    Struggle to get assembly to work.
    I stopped working on this 20 hours ago; just checkpointing old work
    here.

--- programs/noc/BroadcastMemory/broadcast_memory_noc.c
+++ programs/noc/BroadcastMemory/broadcast_memory_noc.c
@@ -1,7 +1,9 @@
 #include <stdint.h>
 #include <flexpret_io.h>
-#include <noc/flexpret_noc_c_api.h>
-#include "asm-gen/hello.h"
+// #include <noc/flexpret_noc_c_api.h>
+
+// #include "asm-gen/hello.h"
+#include "asm-gen/flexpret-noc-c-api.h"
 
 /* | NE | _ | N | E | _ | */
 
@@ -17,41 +19,41 @@ int main2();
 int main3();
 
 int main() {
-    hello(41);
-    // int core_id = read_csr(CSR_COREID);
-    // switch(core_id) {
-    //     case 0: main0(); break;
-    //     case 1: main1(); break;
-    //     case 2: main2(); break;
-    //     case 3: main3(); break;
-    //     default: _fp_print(666); //ERROR
-    // }
+    // hello(41);
+    int core_id = read_csr(CSR_COREID);
+    switch(core_id) {
+        case 0: main0(); break;
+        case 1: main1(); break;
+        case 2: main2(); break;
+        case 3: main3(); break;
+        default: _fp_print(666); //ERROR
+    }
 }
 
-// int main0() {
-//     broadcast_count(0, 125);
-//     read_n_words_and_print(1, EAST_INT);
-//     read_n_words_and_print(2, NORTH_INT);
-//     read_n_words_and_print(3, NORTHEAST_INT);
-// }
+int main0() {
+    broadcast_count(0, 125);
+    read_n_words_and_print(1, EAST_INT);
+    read_n_words_and_print(2, NORTH_INT);
+    read_n_words_and_print(3, NORTHEAST_INT);
+}
 
-// int main1() {
-//     read_n_words_and_print(0, EAST_INT);
-//     broadcast_count(1, 17);
-//     read_n_words_and_print(2, NORTHEAST_INT);
-//     read_n_words_and_print(3, NORTH_INT);
-// }
+int main1() {
+    read_n_words_and_print(0, EAST_INT);
+    broadcast_count(1, 17);
+    read_n_words_and_print(2, NORTHEAST_INT);
+    read_n_words_and_print(3, NORTH_INT);
+}
 
-// int main2() {
-//     read_n_words_and_print(0, NORTH_INT);
-//     read_n_words_and_print(1, NORTHEAST_INT);
-//     broadcast_count(2, 42);
-//     read_n_words_and_print(3, EAST_INT);
-// }
+int main2() {
+    read_n_words_and_print(0, NORTH_INT);
+    read_n_words_and_print(1, NORTHEAST_INT);
+    broadcast_count(2, 42);
+    read_n_words_and_print(3, EAST_INT);
+}
 
-// int main3() {
-//     read_n_words_and_print(0, NORTHEAST_INT);
-//     read_n_words_and_print(1, NORTH_INT);
-//     read_n_words_and_print(2, EAST_INT);
-//     broadcast_count(3, 3);
-// }
+int main3() {
+    read_n_words_and_print(0, NORTHEAST_INT);
+    read_n_words_and_print(1, NORTH_INT);
+    read_n_words_and_print(2, EAST_INT);
+    broadcast_count(3, 3);
+}
